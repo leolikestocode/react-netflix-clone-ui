031c07718947b363508fe6a11903d1fea1c64c7c
leolikestocode <leonardofpereira25@gmail.com>
2021-05-19 22:11:05 -0300

    Insert build folder

---
+++ build/precache-manifest.fa075e201f7ed0ce9a6a51d101fdc7ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec8dca7e9c219d1600ec7f63ae6c912",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0fad593fdc3b6303b5a",
+    "url": "/static/css/main.e8431f7c.chunk.css"
+  },
+  {
+    "revision": "7ec5b9d0aceeeb5b1a8c",
+    "url": "/static/js/2.d1c91c3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.d1c91c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fad593fdc3b6303b5a",
+    "url": "/static/js/main.32098916.chunk.js"
+  },
+  {
+    "revision": "be72ed0f6359687792f7",
+    "url": "/static/js/runtime-main.2a9f0eb6.js"
+  }
+]);
